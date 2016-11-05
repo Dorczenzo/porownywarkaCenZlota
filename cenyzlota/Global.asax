3f16b6fae10923f200cf588bf8aee403959b28ae
Dorczenzo <dorian.loewe@gmail.com>
2016-11-05 17:38:24 +0100

    Add project files.

---
+++ cenyzlota/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cenyzlota.MvcApplication" Language="C#" %>
---
+++ cenyzlota/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cenyzlota.Startup))]
+namespace cenyzlota
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
